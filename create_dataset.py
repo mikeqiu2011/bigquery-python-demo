c5d918f8f721e7dec7633173057f68d890bf2370
mikeqiu2011 <mikeqiu2011@163.com>
2022-02-26 15:02:05 +0800

    initial commit

---
+++ create_dataset.py
@@ -0,0 +1,16 @@
+from google.cloud import bigquery
+
+SERVICE_ACCOUNT_JSON = r'/Users/mikeqiu/keys/valued-lyceum-341108-b090826f2edc.json'
+
+client = bigquery.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
+
+dataset_id = 'valued-lyceum-341108.dataset_py'
+
+dataset = bigquery.Dataset(dataset_id)
+
+dataset.location = 'US'
+dataset.description = 'dataset from Python'
+
+dataset_ref = client.create_dataset(dataset, timeout=30)
+
+print('successful created dataset {}.{}'.format(client.project, dataset_ref.dataset_id))
